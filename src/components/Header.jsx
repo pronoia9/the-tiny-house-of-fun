1045858915703356f58665c9a5c9b1d81b2d28d1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-19 18:14:57 +0300

    convert header to scss + format

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,15 @@
 import CourseButton from './CourseButton';
-import './Header.css';
+import './Header.scss';
 
-const Header = () => {
-  return (
-    <div className='header__wrapper'>
-      <a href='https://designcode.io/' target='_blank' rel='noreferrer'>
-        <p className='header__caption'>login</p>
-      </a>
-      <a href='https://designcode.io/spline2-game-controls' target='_blank' rel='noreferrer'>
-        <CourseButton title='take course' />
-      </a>
-    </div>
-  );
-};
+const Header = () => (
+  <div className='header__wrapper'>
+    <a href='https://designcode.io/' target='_blank' rel='noreferrer'>
+      <p className='header__caption'>login</p>
+    </a>
+    <a href='https://designcode.io/spline2-game-controls' target='_blank' rel='noreferrer'>
+      <CourseButton title='take course' />
+    </a>
+  </div>
+);
 
 export default Header;
