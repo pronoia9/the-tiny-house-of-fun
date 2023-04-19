29a75a6baf3f7642cc33b8b197b01f65145c9ab1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-19 18:10:19 +0300

    convert to scss + format

--- src/components/CloseButton.jsx
+++ src/components/CloseButton.jsx
@@ -1,15 +1,12 @@
 import ButtonImage from '../images/close-button.png';
 import CloseIcon from '../images/close-icon.png';
-import './CloseButton.css';
+import './CloseButton.scss';
 
-const CloseButton = (props) => {
-  const { isOpen, onClick } = props;
-  return (
-    <div className='close-button__wrapper closeButton' isOpen={isOpen}>
-      <img className='close-button__image' src={ButtonImage} alt='close-button' />
-      {isOpen && <img className='close-button__icon' onClick={onClick} src={CloseIcon} alt='close-icon' />}
-    </div>
-  );
-};
+const CloseButton = ({ isOpen, onClick }) => (
+  <div className='close-button__wrapper closeButton' isOpen={isOpen}>
+    <img className='close-button__image' src={ButtonImage} alt='close-button' />
+    {isOpen && <img className='close-button__icon' onClick={onClick} src={CloseIcon} alt='close-icon' />}
+  </div>
+);
 
 export default CloseButton;
