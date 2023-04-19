38f396f79a493d926dad374b0951f8e6ff8c5319
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-19 20:56:09 +0300

    remove styled components + separate controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,52 @@
 import { useState } from 'react';
 
-import Logo from './components/Logo';
-import DownloadButton from './components/DownloadButton';
-import PlayButton from './components/PlayButton';
-import Header from './components/Header';
 import CloseButton from './components/CloseButton';
+import Controls from './components/Controls';
+import DownloadButton from './components/DownloadButton';
+import Header from './components/Header';
+import Logo from './components/Logo';
+import PlayButton from './components/PlayButton';
 import SplineScene from './components/SplineScene';
-import { Wrapper, TextContainer, Title, Description, ButtonContainer, GradientLine, ControlContainer1, ControlContainer2, Subtitle, ControlImage, ContentWrapper, Container, SplineContainer, } from './utils/StyledComponents';
-import ImageControl from './assets/controls.png';
 import DownloadFile from './files/the_tiny_house_of_fun.spline.zip';
+import './App.scss';
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Wrapper className='App'>
+    <div className='app__wrapper'>
       <Logo />
-      <ContentWrapper>
-        <Container>
-          <TextContainer>
-            <Title>3D Room with Game Controls</Title>
-            <Description>
+
+      <div className='app__content__wrapper'>
+        <div className='app__container'>
+          <div className='app__text__container'>
+            <h1 className='app__title'>3D Room with Game Controls</h1>
+            <p className='app__description'>
               Learn how to create a 3D scene in Spline with game interactions like moving, jumping, mouse
               events and dark mode state.
-            </Description>
-          </TextContainer>
+            </p>
+          </div>
 
-          <ButtonContainer>
+          <div className='button__container'>
             <DownloadButton file={DownloadFile} />
             <PlayButton onClick={() => setIsOpen(true)} />
-          </ButtonContainer>
+          </div>
 
-          <GradientLine />
+          <div className='gradient-line' />
 
-          <ControlContainer1>
-            <Subtitle>Keyboard Key</Subtitle>
-            <ControlImage src={ImageControl} alt='control-image' />
-          </ControlContainer1>
-        </Container>
+          <Controls />
+        </div>
 
-        <SplineContainer isOpen={isOpen}>
+        <div className={`spline__container${isOpen ? ' isOpen' : ''}`}>
           <Header />
           <CloseButton isOpen={isOpen} onClick={() => setIsOpen(false)} />
           <SplineScene />
-        </SplineContainer>
+        </div>
 
-        <ControlContainer2>
-          <Subtitle>Keyboard Key</Subtitle>
-          <ControlImage src={ImageControl} alt='control-image' />
-        </ControlContainer2>
-      </ContentWrapper>
-    </Wrapper>
+        <Controls />
+      </div>
+    </div>
   );
 };
+
 export default App;
