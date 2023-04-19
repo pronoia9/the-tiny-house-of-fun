d1adeda8ba5037d32c6437b35ff9307c1056d422
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-19 20:55:51 +0300

    separate controls

---
+++ src/components/Controls.jsx
@@ -0,0 +1,10 @@
+import ImageControl from '../assets/controls.png';
+
+const Controls = () => (
+  <div className='controls__container'>
+    <p className='controls__subtitle'>Keyboard Key</p>
+    <img className='controls__image' src={ImageControl} alt='control-image' />
+  </div>
+);
+
+export default Controls;
