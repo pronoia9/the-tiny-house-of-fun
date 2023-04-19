94e56d7acb61dcfab437802bd5690b2c29349430
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-20 00:45:50 +0300

    fix spline container

--- src/utils/StyledComponents.js
+++ src/utils/StyledComponents.js
@@ -6,6 +6,61 @@ const Wrapper = styled.div`
   overflow: hidden;
 `;
 
+const ContentWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto 855px;
+  max-width: 1440px;
+  width: 100%;
+  margin: 0 auto;
+
+  @media (max-width: 1400px) {
+    grid-template-columns: 1fr;
+    align-content: center;
+    padding-bottom: 40px;
+  }
+`;
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const SplineContainer = styled.div`
+  position: ${(props) => (props.isOpen ? 'fixed' : 'relative')};
+  padding: ${(props) => (props.isOpen ? '0px' : '24px 20px 24px 0px')};
+
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  overflow: auto;
+
+  .spline {
+    border-radius: ${(props) => (props.isOpen ? '0px' : '20px')};
+  }
+
+  .closeButton {
+    top: 54%;
+    left: -32px;
+  }
+
+  @media (max-width: 1400px) {
+    padding: ${(props) => (props.isOpen ? '0px' : '0px 20px 10px')};
+
+    .closeButton {
+      top: -44px;
+      left: 50%;
+      margin-left: -50px;
+      transform: rotate(90deg);
+    }
+  }
+`;
+
+const LogoContainer = styled.div`
+  position: absolute;
+  top: 44px;
+  left: 0px;
+`;
+
 const TextContainer = styled.div`
   display: grid;
   gap: 24px;
@@ -68,7 +123,6 @@ const GradientLine = styled.div`
 
 const ControlContainer1 = styled.div`
   display: block;
-
   @media (max-width: 1400px) {
     display: none;
   }
@@ -76,7 +130,6 @@ const ControlContainer1 = styled.div`
 
 const ControlContainer2 = styled.div`
   display: none;
-
   @media (max-width: 1400px) {
     display: block;
   }
@@ -98,54 +151,6 @@ const ControlImage = styled.img`
 
   @media (max-width: 560px) {
     padding: 8px 20px;
-  }
-`;
-
-const ContentWrapper = styled.div`
-  display: grid;
-  grid-template-columns: auto 855px;
-  max-width: 1440px;
-  width: 100%;
-  margin: 0 auto;
-
-  @media (max-width: 1400px) {
-    grid-template-columns: 1fr;
-    align-content: center;
-    padding-bottom: 40px;
-  }
-`;
-
-const Container = styled.div`
-  position: relative;
-`;
-
-const SplineContainer = styled.div`
-  position: ${(props) => (props = props.isOpen ? 'fixed' : 'relative')};
-  padding: ${(props) => (props = props.isOpen ? '0px' : '24px 20px 24px 0px')};
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  overflow: auto;
-
-  .spline {
-    border-radius: ${(props) => (props = props.isOpen ? '0px' : '20px')};
-  }
-
-  .closeButton {
-    top: 52%;
-    left: -32px;
-  }
-
-  @media (max-width: 1400px) {
-    padding: ${(props) => (props.isOpen ? '0px' : '0px 20px 10px')};
-
-    .closeButton {
-      top: -44px;
-      left: 50%;
-      margin-left: -50px;
-      transform: rotate(90deg);
-    }
   }
 `;
 
