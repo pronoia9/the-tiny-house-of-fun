da11bebc8f9803ca087b3ec321599da838aee64c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-19 20:56:36 +0300

    fix isopen prop

--- src/components/CloseButton.jsx
+++ src/components/CloseButton.jsx
@@ -3,7 +3,7 @@ import CloseIcon from '../assets/close-icon.png';
 import './CloseButton.scss';
 
 const CloseButton = ({ isOpen, onClick }) => (
-  <div className='close-button__wrapper closeButton' isOpen={isOpen}>
+  <div className={`close-button__wrapper${isOpen ? ' isOpen' : ''}`}>
     <img className='close-button__image' src={ButtonImage} alt='close-button' />
     {isOpen && <img className='close-button__icon' onClick={onClick} src={CloseIcon} alt='close-icon' />}
   </div>
